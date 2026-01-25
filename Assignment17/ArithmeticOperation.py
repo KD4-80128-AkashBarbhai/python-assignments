8c8c7591c3de761fb5082cb98c090f2dbe30dc1a
AkashBarbhai <86231755+KD4-80128-AkashBarbhai@users.noreply.github.com>
2026-01-25 14:55:35 +0530

    Add files via upload
    Assignment 17 uploaded

---
+++ Assignment17/ArithmeticOperation.py
@@ -0,0 +1,20 @@
+print("Insid Module : ",__name__)
+def Add(No1,No2):
+    Ans = 0
+    Ans = No1 + No2
+    return Ans
+
+def Sub(No1,No2):
+    Ans = 0
+    Ans = No1 - No2
+    return Ans
+
+def Mul(No1,No2):
+    Ans = 0
+    Ans = No1 * No2
+    return Ans
+
+def Div(No1,No2):
+    Ans = 0
+    Ans = No1 / No2
+    return Ans                                                                                                    
---
+++ Assignment17/Assignment17_8.py
@@ -0,0 +1,11 @@
+def main():
+    num = int(input("Enter the number : "))
+    while 0>num:
+        for i in range(1,6):
+            print(i)
+            i = i + 1 
+        print()
+
+
+if __name__ == "__main__":
+    main()
